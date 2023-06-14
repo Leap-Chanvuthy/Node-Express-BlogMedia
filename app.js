5db7ad6640234c75a7286812bb380de013ad5445
Elon Thy <115206732+Leap-Chanvuthy@users.noreply.github.com>
2023-06-15 01:59:57 +0700

    homepage and post card

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 const express = require('express');
 const app = express();
 const  mongoose = require('mongoose');
-const PORT = 3000;
+
 
 app.use (express.json());
 app.use (express.static('public'));
 app.set ('view engine' , 'ejs');
 
-const URI = 'mongodb+srv://LeapVuthy:Vuthy0112@vuthy.ifubzha.mongodb.net/Node-Express-Blog?retryWrites=true&w=majority'
-mongoose.connect(URI ,  { useNewUrlParser: true,useUnifiedTopology: true })
+const URI = 'mongodb+srv://LeapVuthy:Vuthy0112@vuthy.ifubzha.mongodb.net/Node-Express-Blog?retryWrites=true&w=majority';
+mongoose.connect( URI,  { useNewUrlParser: true,useUnifiedTopology: true })
     .then (result =>{
-       app.listen (PORT);
+       app.listen (3000);
        console.log ('Database connected successfully');
     })
     .catch((err) =>{
